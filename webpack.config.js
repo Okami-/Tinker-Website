88b7eb9682f1b3dd95e00f923730d37feb56cbcb
Richard Baker <richardbaker@Meow.local>
2018-08-09 10:50:43 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+	devtool: "source-map",
+	entry: "./src/index.js",
+	mode: "development",
+	module: {
+		rules: [
+		{
+			test: /\.(js|jsx)$/,
+			exclude: /(node_modules|bower_components)/,
+			loader: 'babel-loader',
+			options: { presets: ['env'] }
+		},
+		{
+			test: /\.scss$/,
+			loaders: ["style", "css", "sass"]
+		},
+		]
+	},
+	resolve: { extensions: ['*', '.js', '.jsx'] },
+	output : {
+		path: path.resolve(__dirname, "dist/"),
+		publicPath: "/dist",
+		filename: "bundle.js"
+	},
+	devServer: {
+		contentBase: path.join(__dirname, "public/"),
+		port: 3000,
+		publicPath: "http://localhost:3000/dist/",
+		hotOnly: true
+	},
+	plugins: [ new webpack.HotModuleReplacementPlugin() ]
+};
