e6b00c3bfe4d524f0e59faed1aee0a17d5e14901
Rich <richiebkr@gmail.com>
2018-09-04 03:12:17 -0700

    adding config files

---
+++ server/config/nodemailer.js
@@ -0,0 +1,50 @@
+const nodemailer = require('nodemailer');
+let { google } = require('googleapis');
+
+let OAuth2 = google.auth.OAuth2;
+require('dotenv').load();
+
+let oauth2Client = new OAuth2(
+    // ClientID
+    process.env.CLIENTID,
+    // Client Secret
+    process.env.CLIENTSECRET,
+    //Redirect URL
+    "https://developers.google.com/oauthplayground",
+);
+
+oauth2Client.setCredentials({
+    refresh_token: process.env.REFRESHTOKEN
+});
+
+let accessToken = "";
+
+oauth2Client.refreshAccessToken(function(err, tokens) {
+    accessToken = tokens.access_token;
+})
+
+var transport = {
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        type: "OAuth2",
+        user: 'richiebkr@gmail.com',
+        clientId: process.env.CLIENTID,
+        clientSecret: process.env.CLIENTSECRET,
+        refreshToken: process.env.REFRESHTOKEN,
+        accessToken: accessToken
+    }
+};
+
+var transporter = nodemailer.createTransport(transport)
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Server is ready to take messages');
+    }
+});
+
+module.exports = transporter;
---
+++ server/config/passport.js
@@ -0,0 +1,45 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+// =========================================================================
+// passport session setup ==================================================
+// =========================================================================
+// required for persistent login sessions
+// passport needs ability to serialize and unserialize users out of session
+
+// used to serialize the user for the session
+passport.serializeUser(function(username, done) {
+    done(null, username);
+});
+
+// used to deserialize the user
+passport.deserializeUser(function(username, done) {
+    done(null, username);
+});
+// =========================================================================
+// LOCAL LOGIN =============================================================
+// =========================================================================
+// we are using named strategies since we have one for login and one for signup
+// by default, if there was no name, it would just be called 'local'
+passport.use('local-login', new LocalStrategy({
+    // by default, local strategy uses username and password, we will override with email
+    usernameField : 'username',
+    passwordField : 'password',
+    passReqToCallback : true // allows us to pass back the entire request to the callback
+},
+function(req, username, password, done) { // callback with email and password from our form
+    // find a user whose email is the same as the forms email
+    // we are checking to see if the user trying to login already exists
+    client.hmget('user', 'username', 'password', function(err, user) {
+        if (err)
+            return done(err);
+        if(username !== user[0])
+            return done(null, false, req.flash('loginmessage', 'No user found.'));
+        if(password !== user[1])
+            return done(null, false, req.flash('loginmessage', 'Oops! Wrong password.'));
+        return done(null, user);
+    });
+}));
+
+
+module.exports = passport;
