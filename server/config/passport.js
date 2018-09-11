958103ccda46915862450bb51fd6520c4ba5e72b
Richard Baker <richardbaker@Meow.local>
2018-09-11 00:06:34 -0700

    cleanup

--- server/config/passport.js
+++ server/config/passport.js
@@ -22,40 +22,42 @@ passport.deserializeUser(async(id, done) => {
     }
 });
 
-passport.use('local-login', new LocalStrategy({
-    usernameField : 'email',
-    passwordField : 'password',
-    passReqToCallback : true 
-},
-    async (req, email, password, done) => {
-        let user = null;
-        await getAsync('usersDatabase').then((users) => {
-            const currUsers = JSON.parse(users);
-            if (!currUsers || currUsers.length < 1) {
-                client.set('usersDatabase', [
-                {
-                    id: 1,
-                    email: 'richiebkr@gmail.com',
-                    // Just a test bcrypt...
-                    password: '$2a$04$4yQfCo8kMpH24T2iQkw9p.hPjcz10m.FcWmgkOhkXNPSpbwHZ877S',
-                    userName: 'rbaker',
-                },
-            ]);
-        }
+passport.use('local-login', 
+    new LocalStrategy(
+        {
+            usernameField : 'email',
+            passwordField : 'password',
+            passReqToCallback : true 
+        },
+        async (req, email, password, done) => {
+            let user = null;
+            await getAsync('usersDatabase').then((users) => {
+                const currUsers = JSON.parse(users);
 
-        user = currUsers.find(currUser => currUser.email === email);
-        });
+                if (!currUsers || currUsers.length < 1) {
+                    client.set('usersDatabase', [
+                    {
+                        id: 1,
+                        email: 'richiebkr@gmail.com',
+                        // Just a test bcrypt...
+                        password: '$2a$04$4yQfCo8kMpH24T2iQkw9p.hPjcz10m.FcWmgkOhkXNPSpbwHZ877S',
+                        userName: 'rbaker',
+                    },
+                ]);
+            }
 
-        if (!user) {
-            done({ type: 'email', message: 'No such user found' }, false);
-            return;
-        }
-        if (bcrypt.compareSync(password, user.password)) {
-            done(null, { id: user.id, email: user.email, userName: user.userName });
-        } else {
-            done({ type: 'password', message: 'Passwords did not match' }, false);
-        }
-    },
+                user = currUsers.find(currUser => currUser.email === email);
+            });
+            if (!user) {
+                done({ type: 'email', message: 'No user found' }, false);
+                return;
+            }
+            if (bcrypt.compareSync(password, user.password)) {
+                done(null, { id: user.id, email: user.email, userName: user.userName });
+            } else {
+                done({ type: 'password', message: 'Passwords did not match' }, false);
+            }
+        },
     ),
 );
 
